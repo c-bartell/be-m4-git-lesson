7ecba6dd56898166904359cf204c6e7d8c372a53
Curtis Bartell <60277914+c-bartell@users.noreply.github.com>
2021-02-06 17:51:17 -0700

    Remove methods from cart and categories controllers
    Remove destroy method
    
    Remove index method
    
    Remove update

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -10,30 +10,4 @@ class CartsController < ApplicationController
   def show
     @items = @cart.retrieve_items
   end
-
-  def destroy
-    @item = Item.find(params[:item_id])
-    @cart.remove_item(params[:item_id])
-    flash[:success] = "Successfully removed <a href=#{item_path(@item)}>#{@item.title}</a> from your cart."
-    redirect_to cart_path
-  end
-
-  def update
-    case
-    when params[:increment_me]
-      @item = Item.find(params[:increment_me])
-      @cart.add_item(@item.id)
-      flash[:success] = "<a href=#{item_path(@item)}>#{@item.title}</a> Quantity updated: #{@cart.count_of(@item.id)}"
-
-    when params[:decrement_me]
-      @item = Item.find(params[:decrement_me])
-      @cart.decrease_quantity(@item.id)
-      if !@cart.count_of(@item.id)
-        flash[:success] = "Successfully removed <a href=#{item_path(@item)}>#{@item.title}</a> from your cart."
-      else
-        flash[:success] = "<a href=#{item_path(@item)}>#{@item.title}</a> Quantity updated: #{@cart.count_of(@item.id)}"
-      end
-    end
-    redirect_to cart_path
-  end
 end
